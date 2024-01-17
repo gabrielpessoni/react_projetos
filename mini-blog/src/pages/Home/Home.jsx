c6f2a9847f68f7f95479fa3f9b78131520ab6871
Gabriel Pessoni <gabriel.pessoni@gmail.com>
2024-01-17 17:58:48 -0300

    getAll posts

--- mini-blog/src/pages/Home/Home.jsx
+++ mini-blog/src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import styles from "./Home.module.css";
 
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+//components
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const handleSubmit = (e) => {};
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
