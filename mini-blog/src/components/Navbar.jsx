c6fa1719cd8525845a704068fb10eb541d838828
Gabriel Pessoni <gabriel.pessoni@gmail.com>
2024-01-16 22:59:23 -0300

    login a auth

--- mini-blog/src/components/Navbar.jsx
+++ mini-blog/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { user } = useAuthValue();
+  const { logout } = useAuthentication();
 
   return (
     <nav className={styles.navbar}>
@@ -73,6 +74,11 @@ const Navbar = () => {
             Sobre
           </NavLink>
         </li>
+        {user && (
+          <li>
+            <button onClick={logout}>Sair</button>
+          </li>
+        )}
       </ul>
     </nav>
   );
--- mini-blog/src/hooks/useAuthentication.jsx
+++ mini-blog/src/hooks/useAuthentication.jsx
@@ -22,6 +22,7 @@ export const useAuthentication = () => {
     if (cancelled) return;
   }
 
+  //register
   const createUser = async (data) => {
     checkIfIsCancelled();
 
@@ -57,6 +58,46 @@ export const useAuthentication = () => {
     }
   };
 
+  //logout
+  const logout = () => {
+    checkIfIsCancelled();
+    signOut(auth);
+  };
+
+  //login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      console.log(error.message.includes("user-not"));
+
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+      }
+
+      console.log(systemErrorMessage);
+
+      setError(systemErrorMessage);
+    }
+
+    console.log(error);
+
+    setLoading(false);
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -66,5 +107,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout,
+    login,
   };
 };
